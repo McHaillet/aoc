fb18504034f9bf2f69b9b189a69027d0558b277c
McHaillet <martenchaillet@gmail.com>
2025-12-01 14:54:35 +0100

    2025 day 01 solution

--- 2025/01/solution.py
+++ 2025/01/solution.py
@@ -1,15 +1,35 @@
+from pathlib import Path
 
 def read(file):
     with open(file) as f:
         content = (l.strip() for l in f.readlines())
-        content = list(l for l in content if l)  # remove blank
+    content = list((l[0], int(l[1:])) for l in content if l)  # remove blank
+    content = list(-x if l == 'L' else x for l, x in content)
     return content
 
 def part_1(data):
-    pass
+    p = 50
+    counter = 0
+    for x in data:
+        p = (p + x) % 100
+        if p == 0:
+            counter += 1
+    return counter
 
 def part_2(data):
-    pass
+    p = 50
+    counter = 0
+    for x in data:
+        if x > 0:
+            n = p + x
+            p = n % 100
+            counter += n // 100
+        else:  # invert the pointer and invert what we add
+            ip = (100 - p) % 100
+            ip = ip - x
+            counter += ip // 100
+            p = (100 - ip) % 100
+    return counter
 
 def run(file):
     data = read(file)
@@ -19,6 +39,8 @@ def run(file):
 def main():
     files = ("example.txt", "input.txt")
     for f in files:
+        if not Path(f).exists():
+            continue
         print(f"Results for {f}...")
         run(f)
 
